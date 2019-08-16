277f2134148e20004087efca62329779f9a99fe7
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-15 17:34:58 -0700

    fixed babel updated error

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -3843,9 +3843,22 @@ eval("var g;\n\n// This works in non-strict mode\ng = (function() {\n\treturn th
   !*** ./src/js/index.js ***!
   \*************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module '@babel/core'\\n babel-loader@8 requires Babel 7.x (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:580:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:506:25)\\n    at Module.require (internal/modules/cjs/loader.js:636:17)\\n    at require (/Users/solodolo/recipesrch/node_modules/v8-compile-cache/v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (/Users/solodolo/recipesrch/node_modules/babel-loader/lib/index.js:10:11)\\n    at Module._compile (/Users/solodolo/recipesrch/node_modules/v8-compile-cache/v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:699:10)\\n    at Module.load (internal/modules/cjs/loader.js:598:32)\\n    at tryModuleLoad (internal/modules/cjs/loader.js:537:12)\\n    at Function.Module._load (internal/modules/cjs/loader.js:529:3)\\n    at Module.require (internal/modules/cjs/loader.js:636:17)\\n    at require (/Users/solodolo/recipesrch/node_modules/v8-compile-cache/v8-compile-cache.js:161:20)\\n    at loadLoader (/Users/solodolo/recipesrch/node_modules/loader-runner/lib/loadLoader.js:18:17)\\n    at iteratePitchingLoaders (/Users/solodolo/recipesrch/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\\n    at runLoaders (/Users/solodolo/recipesrch/node_modules/loader-runner/lib/LoaderRunner.js:365:2)\\n    at NormalModule.doBuild (/Users/solodolo/recipesrch/node_modules/webpack/lib/NormalModule.js:292:3)\\n    at NormalModule.build (/Users/solodolo/recipesrch/node_modules/webpack/lib/NormalModule.js:438:15)\\n    at Compilation.buildModule (/Users/solodolo/recipesrch/node_modules/webpack/lib/Compilation.js:702:10)\\n    at factory.create (/Users/solodolo/recipesrch/node_modules/webpack/lib/Compilation.js:944:14)\\n    at factory (/Users/solodolo/recipesrch/node_modules/webpack/lib/NormalModuleFactory.js:409:6)\\n    at hooks.afterResolve.callAsync (/Users/solodolo/recipesrch/node_modules/webpack/lib/NormalModuleFactory.js:155:13)\\n    at AsyncSeriesWaterfallHook.eval [as callAsync] (eval at create (/Users/solodolo/recipesrch/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:6:1)\\n    at AsyncSeriesWaterfallHook.lazyCompileHook (/Users/solodolo/recipesrch/node_modules/tapable/lib/Hook.js:154:20)\\n    at resolver (/Users/solodolo/recipesrch/node_modules/webpack/lib/NormalModuleFactory.js:138:29)\\n    at process.nextTick (/Users/solodolo/recipesrch/node_modules/webpack/lib/NormalModuleFactory.js:346:9)\\n    at process._tickCallback (internal/process/next_tick.js:61:11)\");\n\n//# sourceURL=webpack:///./src/js/index.js?");
+"use strict";
+eval("\n\nvar _test = __webpack_require__(/*! ./test */ \"./src/js/test.js\");\n\nvar _test2 = _interopRequireDefault(_test);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\nvar x = 23; // Global app controller\n\nconsole.log('I imported ' + _test2.default + ' from another module called test.js! Variable x is ' + x);\n\n//# sourceURL=webpack:///./src/js/index.js?");
+
+/***/ }),
+
+/***/ "./src/js/test.js":
+/*!************************!*\
+  !*** ./src/js/test.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nconsole.log('Imported module');\nexports.default = 123453333;\n\n//# sourceURL=webpack:///./src/js/test.js?");
 
 /***/ }),
 
