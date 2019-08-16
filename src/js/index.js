5ca7b49d9611b9be7cf8b2652a01b626bc560686
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-15 18:37:35 -0700

    empty state object

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 import Search from './models/Search';
 
+// Global state of the app
+// - Search object
+// - Current recipe object
+// - Shopping list object
+// - Liked recipes
+const state = {};
+
 const search = new Search('pizza');
 console.log(search);
 search.getResults();
