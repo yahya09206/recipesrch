4966893bde543941af42b2354af4705a6e93ba1d
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-15 18:11:17 -0700

    added try catch block

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
 async function getResults(query) {
-	const key = '75d8cd037b7dbc5f872b050a9990aee1';
-	const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-	const recipes = res.data.recipes;
-	console.log(recipes);
+	try {
+
+		const key = '75d8cd037b7dbc5f872b050a9990aee1';
+		const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+		const recipes = res.data.recipes;
+		console.log(recipes);
+	} catch (error){
+		alert(error);
+	}
 }
 getResults('pizza');
-//https://www.food2fork.com/api/search
