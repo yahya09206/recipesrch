10b3a711263e46e65e9fc45bd133d11b50a28dac
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-15 18:43:20 -0700

    setting up for search function

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,25 @@ import Search from './models/Search';
 // - Liked recipes
 const state = {};
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+// Search function
+const controlSearch = () => {
+	// 1) Get query from view
+	const query = 'pizza' // TODO
+
+	if (query) {
+		// 2) New search object and add to state
+		state.search = new Search(query);
+
+		// 3) Prepare UI for results
+
+		// 4) Search for recipes
+		state.search.getResults();
+	}
+}
+
+// Event listener for search button
+document.querySelector('.search').addEventListener('submit', e => {
+	e.preventDefault();
+	controlSearch();
+
+});
