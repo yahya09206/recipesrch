89321b38df15b93636170d423c088dff95cdc8e4
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-19 18:35:25 -0700

    setting up which buttons to show

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -41,8 +41,31 @@ const renderRecipe = recipe => {
 	elements.searchResultsList.insertAdjacentHTML('beforeend', markup);
 };
 
+const createButton = (page, type) => `
+	<button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+	    <svg class="search__icon">
+	        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+	    </svg>
+	    <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+	</button>
+`;
+
 const renderButtons = (page, numResults, resPerPage) => {
 	const Pages = Math.ceil(numResults / resPerPage);
+	let button;
+	if (page === 1 && pages > 1) {
+		// only button to go to next page
+		button = createButton(page, 'next');
+	}else if (page < pages) {
+		// both buttons
+		button =` 
+		${createButton(page, 'prev')};
+		${createButton(page, 'next')};
+		`
+	} else if (page === pages && pages > 1) {
+		// only button to go to previous page
+		button = createButton(page, 'prev');
+	}
 };
 
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
