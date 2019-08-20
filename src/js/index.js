4d33d5060e416fafddbe3478cc3b288600c88394
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-19 19:03:32 -0700

    need to clear results before clicking new page

--- src/js/index.js
+++ src/js/index.js
@@ -40,3 +40,12 @@ elements.searchForm.addEventListener('submit', e => {
 	controlSearch();
 
 });
+
+elements.searchResPages.addEventListener('click', e => {
+	const btn = e.target.closest('.btn-inline');
+	if (btn) {
+		const goToPage = parseInt(btn.dataset.goto, 10);
+		searchView.renderResults(state.search.result, goToPage);
+		console.log(goToPage);
+	}
+});
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -69,7 +69,7 @@ const renderButtons = (page, numResults, resPerPage) => {
 	elements.searchResPages.insertAdjacentHTML('afterbegin', button);
 };
 
-export const renderResults = (recipes, page = 3, resPerPage = 10) => {
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
 	// Render results of current page
 	const start = (page - 1) * resPerPage;
 	const end = page * resPerPage;
