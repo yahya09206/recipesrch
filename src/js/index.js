6368be6eae3415ecdacb4b3a31dfa25cae93cb4b
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-19 18:00:43 -0700

    clearing loader icon after results load

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader } from './views/base';
 
 // Global state of the app
 // - Search object
@@ -22,6 +22,8 @@ const controlSearch = async () => {
 		// 3) Prepare UI for results
 		searchView.clearInput();
 		searchView.clearResults();
+		renderLoader(elements.searchRes);
+
 
 		// 4) Search for recipes
 		await state.search.getResults();
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,16 +1,27 @@
 export const elements = {
 	searchForm: 		document.querySelector('.search'),
 	searchInput: 		document.querySelector('.search__field'),
+	searchRes:     	document.querySelector('.results'),
 	searchResultsList:  document.querySelector('.results__list')
+};
+
+export const elementStrings = {
+	loader: 'loader'
 };
 
 // AJAX spinner
 export const renderLoader = parent => {
 	const loader = `
-		<div class="loader">
+		<div class="${elements.loader}">
 			<svg>
 				<use href="img/icons.svg#icon-cw"></use>
 			</svg>
 		</div>
-	`
+	`;
+	parent.insertAdjacentHTML('afterbegin', loader);
+}
+
+// Clear loader
+export const clearLoader = () => {
+	const loader = document.querySelector(elements.loader);
 }
