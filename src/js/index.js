3ab8e47fcae152042a543c2f076ec5e01f71f7de
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-19 17:11:03 -0700

    fixed path error

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,8 @@ const state = {};
 // Search function
 const controlSearch = async () => {
 	// 1) Get query from view
-	const query = 'pizza' // TODO
+	const query = searchView.getInput();
+	console.log(query);
 
 	if (query) {
 		// 2) New search object and add to state
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,3 @@
-export const add = (a, b) => a + b;
-export const multiply = (a, b) => a * b;
-export const ID = 23;
+import { elements } from 'base';
+
+export const getInput = () => elements.searchInput.value;
