8dd697e70cdc966df010f02adba196739add9db8
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-20 16:59:36 -0700

    removing parens from json data

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -36,9 +36,13 @@ export default class Recipe {
 		const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
 		const newIngredients = this.ingredients.map(el => {
 			// 1) Uniform units
-
+			let ingredient = el.toLowerCase();
+			unitsLong.forEach((unit, i) => {
+				ingredient = ingredient.replace(unit, unitsShort[i]);
+			});
 
 			// 2) Remove parentheses
+			
 
 			// 3) Parse ingredients into count, unit and ingredient
 		});
