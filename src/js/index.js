a4b5ac3f661c2f3b6ee9b20800d61b0da20841a2
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-20 16:26:12 -0700

    fixed syntax error

--- src/js/index.js
+++ src/js/index.js
@@ -68,7 +68,7 @@ const controlRecipe = async () => {
 
 		// Calculate servings and time
 		state.recipe.calcTime();
-		state.recipe.calcServings();
+		state.recipe.calcServing();
 		// Render recipe
 		console.log(state.recipe);
 	}
