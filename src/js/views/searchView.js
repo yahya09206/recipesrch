0d3c1cc347f575efdd934a883e21e1ba87f99b36
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-19 17:50:11 -0700

    results are now showing with reduced title names

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,8 +10,17 @@ export const clearResults = () => {
 	elements.searchResultsList.innerHTML = '';
 }
 
-const limitRecipeTitle = (recipe, limit = 17) => {
-	
+const limitRecipeTitle = (title, limit = 17) => {
+	const newTitle = [];
+	if (title.length > limit) {
+		title.split(' ').reduce((acc, cur) => {
+			if (acc + cur.length <= limit) {
+				newTitle.push(cur);
+			}
+			return acc + cur.length;
+		}, 0);
+	} 
+	return `${newTitle.join('')} ...`;
 }
 
 
@@ -23,7 +32,7 @@ const renderRecipe = recipe => {
 	            <img src="${recipe.image_url}" alt="${recipe.title}">
 	        </figure>
 	        <div class="results__data">
-	            <h4 class="results__name">${recipe.title}</h4>
+	            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
 	            <p class="results__author">${recipe.publisher}</p>
 	        </div>
 	    </a>
