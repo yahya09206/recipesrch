dc19e97559a182f44aa9154c3ea0841403398c89
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-19 17:54:13 -0700

    loading spinner made with css

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -2,4 +2,15 @@ export const elements = {
 	searchForm: 		document.querySelector('.search'),
 	searchInput: 		document.querySelector('.search__field'),
 	searchResultsList:  document.querySelector('.results__list')
+};
+
+// AJAX spinner
+export const renderLoader = parent => {
+	const loader = `
+		<div class="loader">
+			<svg>
+				<use href="img/icons.svg#icon-cw"></use>
+			</svg>
+		</div>
+	`
 }
