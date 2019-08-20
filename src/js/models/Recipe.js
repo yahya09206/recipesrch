7961c7c4bba243ff30df4d9964c447dbbd1aba1b
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-20 16:13:07 -0700

    calculating time and servings

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -18,4 +18,15 @@ export default class Recipe {
 			console.log(error);
 		}
 	}
+
+	calcTime() {
+		// Assuming that we need 15 min for each 3 ingredients
+		const numIng = this.ingredients.length;
+		const periods = Math.ceil(numIng / 3);
+		this.time = periods * 15;
+	}
+
+	calcServing() {
+		this.servings = 4;
+	}
 }
