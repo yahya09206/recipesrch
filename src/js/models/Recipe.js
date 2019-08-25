c16b0d9b28f596bdb274b4580c0089ad08f501dd
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-24 18:39:36 -0700

    added kg and grams to units

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -34,6 +34,7 @@ export default class Recipe {
 	parseIngredients() {
 		const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
 		const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+		const units = [...unitsShort, 'kg', 'g'];
 		const newIngredients = this.ingredients.map(el => {
 			// 1) Uniform units
 			let ingredient = el.toLowerCase();
@@ -47,7 +48,7 @@ export default class Recipe {
 			// 3) Parse ingredients into count, unit and ingredient
 				// Test if there is a unit inside string
 				const arrIng = ingredient.split(' ');
-				const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+				const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
 				let objIng;
 				if (unitIndex > -1) {
